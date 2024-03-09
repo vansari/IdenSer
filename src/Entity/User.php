68e872d73095d313ef9e9b78973448b0288190f8
vansari <kurt.junker@posteo.de>
2024-03-09 20:57:38 +0100

    making some changes

---
+++ src/Entity/User.php
@@ -0,0 +1,158 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use App\Repository\UserRepository;
+use App\State\PasswordHashProcessor;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Attribute\Groups;
+use Symfony\Component\Serializer\Attribute\SerializedName;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+#[UniqueEntity(fields: ['email'], message: 'user_already_exists')]
+#[ApiResource(
+    operations: [
+        new Get(
+            security: "'is_granted('ROLE_ADMIN')' or object == user",
+        ),
+        new GetCollection(
+            security: "'is_granted('ROLE_ADMIN')' or object == user",
+        ),
+        new Post(
+            uriTemplate: '/registration',
+            denormalizationContext: ['groups' => ['user:create']],
+            processor: PasswordHashProcessor::class
+        ),
+        new Patch(
+            uriTemplate: '/users/{id}/resetPassword',
+            denormalizationContext: ['groups' => ['user:update']],
+            security: "'is_granted('ROLE_ADMIN')' or object == user",
+            processor: PasswordHashProcessor::class
+        ),
+        new Delete(
+            security: "'is_granted('ROLE_ADMIN')'",
+        ),
+    ],
+    normalizationContext: ['groups' => ['user:read']]
+)]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180, unique: true)]
+    #[Groups(['user:create',])]
+    #[Assert\Email]
+    private ?string $email = null;
+
+    #[ORM\Column]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
+
+
+    #[Groups(['user:create','user:update',])]
+    #[SerializedName('password')]
+    private ?string $plainPassword = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getPlainPassword(): ?string
+    {
+        return $this->plainPassword;
+    }
+
+    public function setPlainPassword(?string $plainPassword): static
+    {
+        $this->plainPassword = $plainPassword;
+
+        return $this;
+    }
+}
---
+++ src/State/PasswordHashProcessor.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace App\State;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProcessorInterface;
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+readonly class PasswordHashProcessor implements ProcessorInterface
+{
+    public function __construct(
+        private ProcessorInterface $processor,
+        private UserPasswordHasherInterface $passwordHasher
+    ) {
+    }
+
+    /**
+     * @param User $data
+     */
+    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): User
+    {
+        if (!$data instanceof User || !$data->getPlainPassword()) {
+            return $this->processor->process($data, $operation, $uriVariables, $context);
+        }
+
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $data,
+            $data->getPlainPassword()
+        );
+
+        $data->setPassword($hashedPassword);
+        $data->eraseCredentials();
+
+        return $this->processor->process($data, $operation, $uriVariables, $context);
+    }
+}
